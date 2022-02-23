c0e21158d8e2b43fca6ebdd0f235add4116b9c1d
bogdanaksenov <bogdanaks@bk.ru>
2022-02-23 12:58:45 +0300

    Base functions in erc20

---
+++ contracts/ERC20.sol
@@ -0,0 +1,60 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.11;
+
+contract ERC20 {
+    constructor() {
+    }
+
+    function transfer() {
+
+    }
+
+    function transferFrom() {
+
+    }
+
+    function approve() {
+
+    }
+
+    function increaseAllowance() {
+
+    }
+
+    function decreaseAllowance() {
+
+    }
+
+    function mint() {
+
+    }
+
+    function burn() {
+        
+    }
+
+    // VIEW FUNCTIONS
+    function name() public view returns (string) {
+
+    }
+
+    function symbol() public view returns (string) {
+
+    }
+
+    function decimals() public view returns (uint8) {
+
+    }
+
+    function balanceOf(address user) public view returns (uint256) {
+
+    }
+
+    function totalSupply() public view returns (uint256) {
+
+    }
+
+    function allowance(address owner, address spender) public view returns (uint256) {
+
+    }
+}
